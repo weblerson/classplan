76c83232f61cc80eb3a7be71cfd2fb5de080449c
weblerson <zonejr4@gmail.com>
2023-04-07 15:46:38 -0400

    changed environment file

--- core/settings.py
+++ core/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from utils import Utils
 from datetime import timedelta
+
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = Utils.read_toml('settings', 'SECRET_KEY')
-SECRET_KEY_JWT = Utils.read_toml('settings', 'SECRET_KEY_JWT')
+SECRET_KEY = config('SECRET_KEY', cast=str)
+SECRET_KEY_JWT = config('SECRET_KEY_JWT', cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Utils.read_toml('settings', 'DEBUG')
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -151,3 +152,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "authentication.User"
--- utils.py
+++ utils.py
@@ -1,11 +1,3 @@
-import tomllib
-
-
 class Utils:
 
-    @staticmethod
-    def read_toml(key: str, value: str) -> str:
-        with open('env.toml', 'rb') as fs:
-            data = tomllib.load(fs)
-
-            return data[key][value]
+    ...
