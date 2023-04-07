1b4c3d9896385ff9100535adb16fbf777ba1f13e
weblerson <zonejr4@gmail.com>
2023-04-07 13:40:44 -0400

    authentication app creation

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # External
+    'rest_framework',
+
+    # My Apps
+    'authentication',
 ]
 
 MIDDLEWARE = [
