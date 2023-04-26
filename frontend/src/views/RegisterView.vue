b5839f3d9fe07037c16811b903cfe56a999f9ea8
weblerson <zonejr4@gmail.com>
2023-04-26 16:50:35 -0400

    fix:(frontend:register) fixed RegisterView background image

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,13 +1,24 @@
 <template>
-
+  <div class="background-image">
+  </div>
 </template>
 
 <script>
 export default {
-    name: "RegisterView"
+    name: "RegisterView",
 }
+
+document.title = 'Cadastre-se';
 </script>
 
 <style scoped>
+.background-image {
+    background-image: url('../assets/img/registerbackground.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
 
+    width: 90vw;
+    height: 100vh;
+}
 </style>
