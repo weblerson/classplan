39de72cecc8d541e951a77b5e7a7ab63a2c63934
weblerson <zonejr4@gmail.com>
2023-04-28 14:34:25 -0400

    feat: (FormInput.vue) created the FormInput component

---
+++ frontend/src/components/general/FormInput.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="mt-0">
+    <label class="form-label text-black label-font mb-0" for="{{ this.id }}">{{ this.label }}:</label>
+    <input class="form-control" id="{{ this.id }}" type="{{ this.type }}" name="{{ this.name }}" />
+</div>
+</template>
+
+<script>
+export default {
+    name: "FormInput",
+
+    props: {
+        id: {
+            type: String,
+            required: true
+        },
+        label: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
