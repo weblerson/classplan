cd4abac561ad7e3aa442f82746346b4512bcba8a
weblerson <zonejr4@gmail.com>
2023-04-28 16:36:31 -0400

    feat: (RegisterView.vue) insertion of AuthForm on template and insertion of dynamic data for component props

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,14 +1,48 @@
 <template>
-  <div class="background-image">
+  <div class="background-image d-flex justify-content-center align-items-center">
+      <div class="d-flex align-items-center">
+          <AuthForm :title="this.title">
+            <FormInput :is="this.firstInput" v-bind="this.props1"/>
+            <FormInput :is="this.secondInput" v-bind="this.props2" />
+          </AuthForm>
+      </div>
   </div>
 </template>
 
 <script>
-export default {
-    name: "RegisterView",
-}
+import AuthForm from "@/components/general/AuthForm.vue";
+import FormInput from "@/components/general/FormInput.vue";
 
 document.title = 'Cadastre-se';
+
+export default {
+    name: "RegisterView",
+
+    data() {
+        return {
+            title: "Cadastre-se",
+            firstInput: FormInput,
+            secondInput: FormInput,
+            props1: {
+                id: "username",
+                label: "Nome de Usuário",
+                name: "username",
+                type: "text"
+            },
+            props2: {
+                id: "email",
+                label: "E-mail",
+                name: "email",
+                type: "email"
+            }
+        }
+    },
+
+    components: {
+        AuthForm,
+        FormInput
+    }
+}
 </script>
 
 <style scoped>
