53887c6d21214fb1d9fa0262895222c08ccf6bb1
weblerson <zonejr4@gmail.com>
2023-07-26 12:23:55 -0400

    test: written new register signal test

--- backend/authentication/tests/register_signal_tests.py
+++ backend/authentication/tests/register_signal_tests.py
@@ -31,3 +31,13 @@ class RegisterSignalTests(test.APITestCase):
         user_token = UserActivationToken.objects.get(user=user)
 
         self.assertIsNotNone(user_token)
+
+    def test_if_user_token_is_active_by_default(self):
+        """
+        Tests if the user token is active by default
+        """
+
+        user = User.objects.create_user(**self.data)
+        user_token = UserActivationToken.objects.get(user=user)
+
+        self.assertTrue(user_token.active)
