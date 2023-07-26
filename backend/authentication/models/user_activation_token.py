74f6eea7a48ea8683095dc59f90cc4fbc383ad39
weblerson <zonejr4@gmail.com>
2023-07-26 12:18:11 -0400

    feat: 'active' property of UserActivationToken is now true by default

--- backend/authentication/models/user_activation_token.py
+++ backend/authentication/models/user_activation_token.py
@@ -6,7 +6,7 @@ class UserActivationToken(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=64)
-    active = models.BooleanField()
+    active = models.BooleanField(default=True)
 
     def __str__(self) -> str:
         return f'Token de {self.user.username} - {"Usado" if self.active else "Não Usado"}'
