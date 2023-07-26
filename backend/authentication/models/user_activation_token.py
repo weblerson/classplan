5e03540efb5538609999cb360eeea7dac0c8753c
weblerson <zonejr4@gmail.com>
2023-07-26 12:17:34 -0400

    feat: created 'active' property on UserActivationToken model

--- backend/authentication/models/user_activation_token.py
+++ backend/authentication/models/user_activation_token.py
@@ -6,6 +6,7 @@ class UserActivationToken(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=64)
+    active = models.BooleanField()
 
     def __str__(self) -> str:
-        return f'Token de {self.user.username}'
+        return f'Token de {self.user.username} - {"Usado" if self.active else "Não Usado"}'
