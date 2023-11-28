05bbf16010d0f5d32ac47e7f996c3892de273da1
weblerson <zonejr4@gmail.com>
2023-11-28 13:24:12 -0400

    fix: <healthcheck/tests> fixed identation

--- backend/healthcheck/tests.py
+++ backend/healthcheck/tests.py
@@ -6,10 +6,10 @@ from rest_framework import status
 
 class HealthcheckTest(test.APITestCase):
 
-   def test_if_healthcheck_return_200_ok(self):
-       url = reverse('healthcheck')
+    def test_if_healthcheck_return_200_ok(self):
+        url = reverse('healthcheck')
 
-       response = self.client.get(url)
+        response = self.client.get(url)
 
-       self.assertEqual(response.status_code, status.HTTP_200_OK)
-       self.assertEqual(response.data, 'ok')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, 'ok')
