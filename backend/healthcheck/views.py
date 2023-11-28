763a9fd30467acaed5a3abc195479d6c888207b3
weblerson <zonejr4@gmail.com>
2023-11-28 13:25:18 -0400

    fix: <healthcheck/views> fixed permissions

--- backend/healthcheck/views.py
+++ backend/healthcheck/views.py
@@ -1,9 +1,13 @@
 from rest_framework import views
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import permissions
 
 
 class HealthcheckView(views.APIView):
 
-    def get(self, request, *args, **kwargs):
+    permission_classes = [permissions.AllowAny]
+
+    @staticmethod
+    def get(request, *args, **kwargs):
         return Response('ok', status=status.HTTP_200_OK)
