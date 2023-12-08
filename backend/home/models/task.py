f368dce4fe44c99d0d7bbebeddad7e6590ca81ed
weblerson <zonejr4@gmail.com>
2023-12-08 12:46:14 -0400

    feat: created task model

---
+++ backend/home/models/task.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from space import Space
+
+
+class Task(models.Model):
+
+    name = models.CharField(max_length=64, blank=False, default='Sem nome')
+    is_done = models.BooleanField(default=False)
+    space = models.ForeignKey(Space, on_delete=models.CASCADE)
