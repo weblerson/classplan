5e8d9b56c3fe7c7f888aafdb08eea1cf0fb766e6
weblerson <zonejr4@gmail.com>
2023-12-10 17:41:15 -0400

    fix: <home.space> fixed some conflicts

--- backend/home/models/space.py
+++ backend/home/models/space.py
@@ -5,10 +5,10 @@ from authentication.models import User
 
 class Space(models.Model):
 
-    image = models.ImageField(upload_to='/media/home/spaces', default='default.jpg')
+    image = models.ImageField(upload_to='media/home/spaces', default='default/default.jpg')
     title = models.CharField(max_length=32, blank=False, default='Filmes, séries etc.')
     objective = models.TextField(default='Assistir coisas legais')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
     is_private = models.BooleanField(default=True)
     is_personal = models.BooleanField(default=False)
     partners = models.ManyToManyField(User, blank=True)
