a09e6e5d654ee5715872b2d9e3e60401ad91ca11
weblerson <zonejr4@gmail.com>
2023-12-10 17:40:32 -0400

    feat: added new models to Admin

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Space, Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        ('Tarefa', {'fields': ('name', 'is_done')}),
+        ('Relação', {'fields': ('space',)})
+    )
+
+    readonly_fields = ('space',)
+
+
+@admin.register(Space)
+class SpaceAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        ('Informações', {'fields': ('image', 'title', 'objective', 'user')}),
+        ('Privacidade', {'fields': ('is_private', 'is_personal')}),
+        ('Parceiros', {'fields': ('partners',)})
+    )
+
+    readonly_fields = ('user', 'is_personal')
